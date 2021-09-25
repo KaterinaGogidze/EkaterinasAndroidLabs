9960360c8d1e43f0ac1e65a6cff6fcf1633ac8a1
kater <gogi0004@algonquinlive.com>
2021-09-24 22:54:25 -0400

    working on image widgets

--- app/src/main/java/algonquin/cst2335/gogidze/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/gogidze/MainActivity.java
@@ -3,6 +3,7 @@ package algonquin.cst2335.gogidze;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton btn = findViewById(R.id.bottom);
+        btn.setOnClickListener((vw) -> {
+            int width = btn.getWidth();
+            int height = btn.getHeight();
+        });
+
     }
 }
