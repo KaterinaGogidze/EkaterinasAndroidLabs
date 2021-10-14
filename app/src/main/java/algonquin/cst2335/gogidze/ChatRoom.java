95d2cfbaac3d79c4d30a39655a68c2118432a28b
kater <gogi0004@algonquinlive.com>
2021-10-14 12:13:52 -0400

    working on Week5

---
+++ app/src/main/java/algonquin/cst2335/gogidze/ChatRoom.java
@@ -0,0 +1,15 @@
+package algonquin.cst2335.gogidze;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChatRoom extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.chatlayout);
+    }
+}
