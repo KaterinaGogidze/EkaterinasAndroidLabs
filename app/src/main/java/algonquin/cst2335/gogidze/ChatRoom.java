5197f07e98d76d4e2aabd740ab5e1659004f039c
kater <gogi0004@algonquinlive.com>
2021-10-15 22:17:09 -0400

    Finished module 1

--- app/src/main/java/algonquin/cst2335/gogidze/ChatRoom.java
+++ app/src/main/java/algonquin/cst2335/gogidze/ChatRoom.java
@@ -1,15 +1,66 @@
 package algonquin.cst2335.gogidze;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Date;
 
 public class ChatRoom extends AppCompatActivity {
+
+    RecyclerView chatList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.chatlayout);
+        chatList = findViewById(R.id.myrecycler);
+        chatList.setAdapter(new MyChatAdapter());
+    }
+
+    private class MyChatAdapter extends RecyclerView.Adapter {
+
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                return null;
+            }
+
+            @Override
+            public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+            }
+
+            @Override
+            public int getItemCount() {
+                return 0;
+            }
+    }
+
+    private class ChatMessage {
+        String message;
+        int sendOrReceive;
+        Date timeSent;
+
+        public ChatMessage(String message, int sendOrReceive, Date timeSent) {
+            this.message = message;
+            this.sendOrReceive = sendOrReceive;
+            this.timeSent = timeSent;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public int getSendOrReceive() {
+            return sendOrReceive;
+        }
+
+        public Date getTimeSent() {
+            return timeSent;
+        }
     }
 }
