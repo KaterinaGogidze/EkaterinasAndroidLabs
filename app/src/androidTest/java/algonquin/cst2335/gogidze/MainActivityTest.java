eae9c443b4e4b855072ef52f0600b49270c7c9ed
kater <gogi0004@algonquinlive.com>
2021-10-31 00:48:55 -0400

    Added JavaDoc comments and Finished 5 test cases.

---
+++ app/src/androidTest/java/algonquin/cst2335/gogidze/MainActivityTest.java
@@ -0,0 +1,156 @@
+package algonquin.cst2335.gogidze;
+
+
+import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import static androidx.test.InstrumentationRegistry.getInstrumentation;
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+
+import algonquin.cst2335.gogidze.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.editText),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatEditText.perform(click());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.editText),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatEditText2.perform(replaceText("12345"), closeSoftKeyboard());
+
+        pressBack();
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.button), withText("Login"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.textView), withText("You shall not pass!"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        textView.check(matches(withText("You shall not pass!")));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.textView), withText("You shall not pass!"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        textView2.check(matches(withText("You shall not pass!")));
+    }
+
+    @Test
+    public void testFindMissingUpperCase() {
+        // Find the view
+        ViewInteraction appCompatEditText = onView( withId(R.id.editText));
+        // Type in pa$$w0rd
+        appCompatEditText.perform(replaceText("pa$$w0rd"));
+        // Find the button
+        ViewInteraction materialButton = onView(withId(R.id.button));
+        // Click the button
+        materialButton.perform(click());
+        // Find the text view
+        ViewInteraction textView = onView(withId(R.id.textView));
+        // Check the text
+        textView.check(matches(withText("You shall not pass!")));
+    }
+
+    @Test
+    public void testFindMissingLowerCase() {
+        ViewInteraction appCompatEditText = onView(withId(R.id.editText));
+        appCompatEditText.perform(replaceText("PAS123#$%"));
+        ViewInteraction materialButton = onView(withId(R.id.button));
+        materialButton.perform(click());
+        ViewInteraction textView = onView(withId(R.id.textView));
+        textView.check(matches(withText("You shall not pass!")));
+
+    }
+
+    @Test
+    public void testFindMissingSpecialCharacter() {
+        ViewInteraction appCompatEditText = onView(withId(R.id.editText));
+        appCompatEditText.perform(replaceText("Password123"));
+        ViewInteraction materialButton = onView(withId(R.id.button));
+        materialButton.perform(click());
+        ViewInteraction textView = onView(withId(R.id.textView));
+        textView.check(matches(withText("You shall not pass!")));
+    }
+
+    @Test
+    public void testFindMissingDigit() {
+        ViewInteraction appCompatEditText = onView(withId(R.id.editText));
+        appCompatEditText.perform(replaceText("Password!"));
+        ViewInteraction materialButton = onView(withId(R.id.button));
+        materialButton.perform(click());
+        ViewInteraction textView = onView(withId(R.id.textView));
+        textView.check(matches(withText("You shall not pass!")));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
--- app/src/main/java/algonquin/cst2335/gogidze/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/gogidze/MainActivity.java
@@ -3,12 +3,117 @@ package algonquin.cst2335.gogidze;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+/** This page lets the user type in a pass....
+ *
+ * @author Ekaterina Gogidze
+ * @versin 1.0
+ */
 public class MainActivity extends AppCompatActivity {
+
+    /** This holds the text at the centre of the screen */
+    TextView tv = null;
+
+    /** This holds the password text at the centre of the screen */
+    EditText et = null;
+
+    /** This holds the Login button  at the button of the screen */
+    Button btn = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.textView);
+        et = findViewById(R.id.editText);
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener( clk -> {
+            String password = et.getText().toString();
+
+            if (checkPasswordComplexity(password))
+                tv.setText("Your password meets the requirements");
+          else
+              tv.setText("You shall not pass!");
+
+
+        });
+
+
+    }
+
+    /** This function checks if the password has Upper case, Lower Case, Number and Special Character.
+     *
+     * @param pw The String object that we are checking
+     * @return True if all conditions are met, otherwise false.
+     */
+    boolean checkPasswordComplexity(String pw) {
+        boolean foundUpperCase, foundLowerCase, foundNumber, foundSpecial;
+        foundUpperCase = foundLowerCase = foundNumber = foundSpecial = false;
+
+        //Star Looping.
+        for(int i = 0; i < pw.length(); i++) {
+            char c = pw.charAt(i);
+            Log.i("Looking at char:", ""+c);
+            if(Character.isLowerCase(c))
+                foundLowerCase = true;
+            if (Character.isUpperCase(c))
+                foundUpperCase = true;
+            if (Character.isDigit(c))
+                foundNumber = true;
+            else if(isSpecialCharacter(c)) {
+                foundSpecial = true;
+            }
+        }
+
+        if(!foundLowerCase) {
+            Toast.makeText(MainActivity.this, "Missing lower case character", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if(!foundUpperCase) {
+            Toast.makeText(MainActivity.this, "Missing Upper case character", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        else if(!foundNumber) {
+            Toast.makeText(MainActivity.this, "Missing number", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        else if(!foundSpecial) {
+            Toast.makeText(MainActivity.this, "Missing special character", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        //else
+            //return true;
+
+        // If anything is false, then it is not in the password.
+        return foundLowerCase && foundUpperCase && foundNumber && foundSpecial;
+    }
+
+    /** This returns if one of this character {#$%^&*!@?}, false otherwise
+     *
+     * @param c The character we are checking
+     * @return True if c is one of {#$%^&*!@?}, false otherwise
+     */
+    boolean isSpecialCharacter(char c) {
+        switch(c) {
+            case'#':
+            case'$':
+            case'%':
+            case'^':
+            case'&':
+            case'*':
+            case'!':
+            case'@':
+            case'?':
+                return true;
+            default:
+                return false;
+        }
     }
 }
